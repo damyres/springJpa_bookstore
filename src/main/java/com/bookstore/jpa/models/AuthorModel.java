3dadf852fde2c41cd938510affc0cfcf70843619
Damyres <damyresmaciell@gmail.com>
2024-09-14 22:00:46 -0300

    Criar entidade AuthorModel

---
+++ src/main/java/com/bookstore/jpa/models/AuthorModel.java
@@ -0,0 +1,35 @@
+package com.bookstore.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_AUTHOR")
+public class AuthorModel implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false,unique = true)
+    private String nome;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
