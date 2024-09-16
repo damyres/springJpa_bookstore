9b62845b83cf3627f8a4267a31f77d89e3e8e481
Damyres <damyresmaciell@gmail.com>
2024-09-16 18:52:24 -0300

    Criar as classes Services de Book

--- src/main/java/com/bookstore/jpa/services/BookModelService.java
+++ src/main/java/com/bookstore/jpa/services/BookModelService.java
@@ -1,15 +1,45 @@
 package com.bookstore.jpa.services;
 
+import com.bookstore.jpa.dtos.BookRecordDto;
+import com.bookstore.jpa.models.BookModel;
+import com.bookstore.jpa.models.ReviewModel;
+import com.bookstore.jpa.repositories.AuthorRepository;
 import com.bookstore.jpa.repositories.BookModelRepository;
+import com.bookstore.jpa.repositories.PublisherRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
+
 @Service
 public class BookModelService {
-    private final BookModelRepository repository;
+    private final BookModelRepository bookModelRepository;
+    private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
 
     @Autowired
-    public BookModelService(final BookModelRepository repository) {
-        this.repository = repository;
+    public BookModelService(final BookModelRepository bookModelRepository,
+                            final AuthorRepository authorRepository,
+                            final PublisherRepository publisherRepository) {
+        this.bookModelRepository = bookModelRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
+
+    @Transactional //garante um rollback
+    public BookModel save(final BookRecordDto bookRecordDto) {
+        BookModel book = new BookModel();
+        book.setTitle(bookRecordDto.title());
+        book.setPublisher(publisherRepository.findById(bookRecordDto.publishedId()).get());
+        book.setAuthors(authorRepository.findAllById(bookRecordDto.authorIds()).stream().collect(Collectors.toSet()));
+
+        ReviewModel reviewModel = new ReviewModel();
+        reviewModel.setComment(bookRecordDto.reviewComments());
+        reviewModel.setBook(book);
+        book.setReview(reviewModel);
+
+        return bookModelRepository.save(book);
+    }
+
 }
